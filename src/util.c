1f548fce4575265d5c776cb6c4b967a144b51950
haiii <haiii@bd453a55-ea4b-a647-8189-d680511496f7>
2025-09-02 18:39:35 +0000

    fix utils
    git-svn-id: file:///var/svn/fishymail/trunk@45 bd453a55-ea4b-a647-8189-d680511496f7

--- src/util.c
+++ src/util.c
@@ -2,36 +2,31 @@
 
 #include <fishymail.h>
 
+static inline int alnum(char c) {
+	return ('a' <= c && c <= 'z') ||
+	       ('A' <= c && c <= 'Z') ||
+	       ('0' <= c && c <= '9');
+}
+
 void FishyMailRemoveSpecial(const char* from, char* to) {
 	int i;
 	int ind = 0;
 	for(i = 0; from[i] != 0; i++) {
-		if('a' <= from[i] && from[i] <= 'z') {
-			to[ind++] = from[i];
-		} else if('A' <= from[i] && from[i] <= 'Z') {
-			to[ind++] = from[i];
-		} else if('0' <= from[i] && from[i] <= '9') {
-			to[ind++] = from[i];
-		} else if(from[i] == '_' || from[i] == ' ') {
+		if(alnum(from[i]) || from[i] == '_' || from[i] == ' ') {
 			to[ind++] = from[i];
 		}
-		to[ind + 1] = 0;
 	}
+	to[ind] = 0;
 }
 
 void FishyMailSanitizeName(const char* from, char* to) {
 	int i;
 	int ind = 0;
 	for(i = 0; from[i] != 0; i++) {
-		if('a' <= from[i] && from[i] <= 'z') {
-			to[ind++] = toupper((int)from[i]);
-		} else if('A' <= from[i] && from[i] <= 'Z') {
-			to[ind++] = from[i];
-		} else if('0' <= from[i] && from[i] <= '9') {
-			to[ind++] = from[i];
-		} else if(from[i] == '_' || from[i] == ' ') {
-			to[ind++] = from[i];
+		if(alnum(from[i]) || from[i] == '_' || from[i] == ' ') {
+			unsigned char c = (unsigned char)from[i];
+			to[ind++]	= (char)toupper(c);
 		}
-		to[ind + 1] = 0;
 	}
+	to[ind] = 0;
 }
