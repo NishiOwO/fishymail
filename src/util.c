fb202455287fd62f864646796e1761e5f2c044b3
haiii <haiii@bd453a55-ea4b-a647-8189-d680511496f7>
2025-09-02 18:44:50 +0000

    add comment
    git-svn-id: file:///var/svn/fishymail/trunk@46 bd453a55-ea4b-a647-8189-d680511496f7

--- src/util.c
+++ src/util.c
@@ -2,7 +2,8 @@
 
 #include <fishymail.h>
 
-static inline int alnum(char c) {
+// use this instead of isalnum to avoid depending on the current locale
+static inline int is_alnum(char c) {
 	return ('a' <= c && c <= 'z') ||
 	       ('A' <= c && c <= 'Z') ||
 	       ('0' <= c && c <= '9');
@@ -12,7 +13,7 @@ void FishyMailRemoveSpecial(const char* from, char* to) {
 	int i;
 	int ind = 0;
 	for(i = 0; from[i] != 0; i++) {
-		if(alnum(from[i]) || from[i] == '_' || from[i] == ' ') {
+		if(is_alnum(from[i]) || from[i] == '_' || from[i] == ' ') {
 			to[ind++] = from[i];
 		}
 	}
@@ -23,7 +24,7 @@ void FishyMailSanitizeName(const char* from, char* to) {
 	int i;
 	int ind = 0;
 	for(i = 0; from[i] != 0; i++) {
-		if(alnum(from[i]) || from[i] == '_' || from[i] == ' ') {
+		if(is_alnum(from[i]) || from[i] == '_' || from[i] == ' ') {
 			unsigned char c = (unsigned char)from[i];
 			to[ind++]	= (char)toupper(c);
 		}
